58bdc814ba6d83970b4815417699f497cc457c8a
Domink <theprogson@gmail.com>
2023-12-17 03:48:09 +0100

    it works XD, added donation socket

--- main.py
+++ main.py
@@ -1,20 +1,75 @@
-#import shutil
 import requests
 import sys
 import json
 import os
 import subprocess
+import socketio
+
 
 client_id = os.getenv("STREAMLAB_TAPETYPRO_CLIENT_ID")
 client_secret = os.getenv("STREAMLAB_TAPETYPRO_CLIENT_SECRET")
+    
+def get_access_token(code):    
+    data = {
+    "grant_type": "authorization_code",
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "redirect_uri": "http://127.0.0.1:80",
+    "code": code
+    }
+    url = "https://streamlabs.com/api/v2.0/token"
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response_dict = json.loads(response.text)
+    return response_dict.get('access_token', None)   
 
-print("dzialam")
+def get_socket_token(access_token):  
+    url = "https://streamlabs.com/api/v2.0/socket/token"
+    headers = {"accept": "application/json",'Authorization': f'Bearer {access_token}'}
 
-lastID = 0
-access_token = ""
+    response = requests.get(url, headers=headers)
+    try:
+        response_dict = json.loads(response.text)
+        return response_dict.get('socket_token', None)
+    except json.JSONDecodeError as e:
+        print("nie dziala")
+        print(response.text)
+        return None
 
-def get_donations( limit = 0, after = 0):
-    global access_token
+
+sio = socketio.Client(logger=True, engineio_logger=True)
+@sio.event
+def connect():
+    print('Connection established')
+
+@sio.event
+def disconnect():
+    print('Disconnected from server')
+
+@sio.on('event')
+def on_event(eventData):
+    if eventData['type'] == 'donation':
+        donation_data = eventData['message'][0]  
+        donation_id = donation_data['id']  
+        donation_message = donation_data['message']
+        donation_amount = donation_data['amount'] 
+        donation_currency = donation_data['currency']
+        generate_wallpaper(donation_id,donation_message)
+
+
+@sio.on('connect')
+def connect():
+    print('p z serwerem socket.io')
+
+
+@sio.on('message')
+def event(data):
+    print("hejka")
+    
+def get_donations( access_token,limit = 0, after = 0):
     url = "https://streamlabs.com/api/v2.0/donations"
     if limit:
         url = f"https://streamlabs.com/api/v2.0/donations?limit={limit}"
@@ -24,29 +79,11 @@ def get_donations( limit = 0, after = 0):
     headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(url, headers=headers)
     
-    
+       
     if response.status_code == 200:
         return response.json()
     else:
         return response.text 
-    
-def process_code(code):
-    data = {
-    "grant_type": "authorization_code",
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "redirect_uri": "http://127.0.0.1:80",
-    "code": code
-    }
-    url = 'https://streamlabs.com/api/v2.0/token'
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    response_dict = json.loads(response.text)
-    global access_token
-    access_token = response_dict.get('access_token', None)
 
 def zapisz_elementy_json(donation_dict, katalog_donacji="donejty"):
     sciezka_katalogu = os.path.dirname(os.path.abspath(__file__)) + "/" + katalog_donacji
@@ -65,9 +102,10 @@ def zapisz_elementy_json(donation_dict, katalog_donacji="donejty"):
 
             print(f"Zapisano: {nazwa_pliku}")
     else:
-        print("Brak 'data' w słowniku JSON.")
+        print("Brak 'data' w dictionary JSON.")
 
 '''def move_donation_to_otherFolder():
+
     source_folder = os.path.dirname(os.path.abspath(__file__)) + "/donejty"
     destination_folder = os.path.dirname(os.path.abspath(__file__)) + "/../TapetyPro/donejty"
     
@@ -79,22 +117,27 @@ def zapisz_elementy_json(donation_dict, katalog_donacji="donejty"):
             shutil.move(source_path, destination_path)
             print(f'Przeniesiono: {source_path} do {destination_path}')'''
 
+
 def generate_wallpaper(donation_id,prompt):
     path_to_generate_wallpaper_script = os.path.join(os.path.dirname(os.path.abspath(__file__)) ,"generate_wallpaper.py")
     result = subprocess.run([sys.executable, path_to_generate_wallpaper_script,str(donation_id), str(prompt)], capture_output=True, text=True)
     return result.returncode        
+       
     
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         code = sys.argv[1]
-        process_code(code)
+        access_token = get_access_token(code)
+        socket_token = get_socket_token(access_token)
+        #print(socket_token)
         
-        #donation_dict = get_donations(limit =  0)
-        #lastID = (donation_dict.get('data',None)[-1].get('donation_id',None))
-        #zapisz_elementy_json(donation_dict)
-        #move_donation_to_otherFolder()
-        result = generate_wallpaper(1, "czerwony pies z wielkim krokodylim czolem")
-        
+        sio.connect(f'https://sockets.streamlabs.com?token={socket_token}', transports=['websocket'])
+
+        try:
+            sio.wait()
+        except KeyboardInterrupt:
+            pass
+
     else:
         print("Nie otrzymano kodu")
 
